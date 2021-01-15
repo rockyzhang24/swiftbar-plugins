6add0f6bfd11bbb73e7ec6e2a028ad198b4584ec
Yan Zhang <yanzhang0219@gmail.com>
2021-01-14 19:46:04 -0800

    bug fix: wrong color position when space id is a double-digit number

--- yabai/yabai.365d.sh
+++ yabai/yabai.365d.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # <bitbar.title>Yabai Stats</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.version>v1.1</bitbar.version>
 # <bitbar.author>Rocky Zhang</bitbar.author>
 # <bitbar.author.github>yanzhang0219</bitbar.author.github>
-# <bitbar.desc>Display the workspaces on each monitor and the layout of the current workspace</bitbar.desc>
+# <bitbar.desc>Display the workspace ids on each monitor and the layout of the current workspace</bitbar.desc>
 # <bitbar.dependencies>yabai, skhd, jq</bitbar.dependencies>
 
 # SwiftBar needs this
@@ -21,18 +21,18 @@ yabai_spaces=$(yabai -m query --spaces)
 # The workspace id that is currently focused
 space_focused=$(echo ${yabai_spaces} | jq '.[] | select(.focused == 1) | .index')
 
-# All the ids of visible workspaces not including the focused one (each non-focused monitor has one visible workspace)
-spaces_visible=$(yabai -m query --spaces | jq -rj '.[] | select(.visible == 1 and .focused != 1) | .index')
-
 # Workspaces for each monitor and color the focused workspace
 # x | x x | x ---> means totally three monitors, the left one has one workspace, the middle one has two, and the right one has one
-spaces=$(yabai -m query --displays | jq -r 'sort_by(.frame.x) | map(.spaces | join(" ")) | join(" │ ")' \
-         | sed -r "s/${space_focused}/${COLOR_FOCUSED}${space_focused}${RESET}/")
+spaces=$(yabai -m query --displays | jq -r 'sort_by(.frame.x) | map(.spaces | join(" ")) | join(" │ ")' | \
+         sed -r "s/(([[:space:]]|^)${space_focused}([[:space:]]|\$))/${COLOR_FOCUSED}\1${RESET}/")
 
 # Color the visible workspaces
-if [[ -n ${spaces_visible} ]]; then
-  spaces=$(echo ${spaces} | sed -r "s/([${spaces_visible}][[:space:]]|[${spaces_visible}]\$)/${COLOR_VISIBLE}\1${RESET}/g")
-fi
+# Get all the ids of visible workspaces not including the focused one (each non-focused monitor has one visible workspace),
+# and for each id, color it in the output
+while read -r id
+do
+  spaces=$(echo ${spaces} | sed -r "s/(([[:space:]]|^)${id}([[:space:]]|\$))/${COLOR_VISIBLE}\1${RESET}/g")
+done < <(yabai -m query --spaces | jq -r '.[] | select(.visible == 1 and .focused != 1) | .index')
 
 # The layout of the focused workspace: B for bsp, F for float and S for stack
 cur_space_type=$(echo ${yabai_spaces} | jq -r '.[] | select(.focused == 1) | .type | if . == "bsp" then "B" elif . == "float" then "F" elif . == "stack" then "S" else . end')
